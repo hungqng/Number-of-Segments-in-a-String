ccc5af75469ca84a80331f76f4ca7f7bbbeed137
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-22 19:54:22 -0700

    Add files via upload

---
+++ Number of Segments in a String.py
@@ -0,0 +1,9 @@
+# 434. Number of Segments in a String
+
+# Given a string s, return the number of segments in the string.
+
+# A segment is defined to be a contiguous sequence of non-space characters.
+
+class Solution:
+    def countSegments(self, s: str) -> int:
+        return len(s.split())
