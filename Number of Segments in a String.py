ea9f552d44a7c9dd2304afb7f8d7e39ed6a61634
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-05-22 19:55:11 -0700

    Add files via upload

--- Number of Segments in a String.py
+++ Number of Segments in a String.py
@@ -7,3 +7,13 @@
 class Solution:
     def countSegments(self, s: str) -> int:
         return len(s.split())
+
+        # Solution 2 not using split
+        if len(s.strip()) < 1:
+            return 0
+        
+        count = 0
+        for i in range(len(s.strip()) - 1):
+            if s[i] == " " and s[i + 1] != " " :
+                count += 1
+        return count + 1
